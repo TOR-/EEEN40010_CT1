26e3e11a2c12f2b8ed1a2cc0cba58ad201f60f8b
Tiarnach O Riada <tiarnachreidy@gmail.com>
2020-11-15 18:42:42 +0000

    What's done is done, what's not is not. This is what I've got

--- p2.m
+++ p2.m
@@ -21,32 +21,40 @@ D = [0];
 Gp = ss(A,B,C,D);
 %% 4 P I? D? Controller
 % PI
-k = 23;
+K = 23;
 z = 0.5;
-Gc = tf(k*[1 z],[1 0]);
+Gc = tf(K*[1 z],[1 0]);
 Go = series(Gp, Gc);
 rlocus(Go)
 %rlocfind(Go) % z=0.6, k=32
 Gideal = tf(1,1);
 Gcl = feedback(Go, Gideal);
-step(Gcl)
+%step(Gcl)
 title("Step response of closed loop local LTI system")
 %print('report/img/p2-step-Gcl','-dpng');
 %% 6 i Fail low?
-kl = k*0.01;
+kl = K*0.01;
 Gclow = tf(kl*[1 z],[1 0]);
 Golow = series(Gp, Gclow);
 Gcllow = feedback(Golow, Gideal);
-step(Gcllow)
+%step(Gcllow)
 title("Step response of fail low case")
 %print('report/img/p2-step-fail-low','-dpng');
 %% 6 ii Fail high?
-kh = k*5;
+kh = K*5;
 Gchigh = tf(kh*[1 z],[1 0]);
 Gohigh = series(Gp, Gchigh);
 Gclhigh = feedback(Gohigh, Gideal);
-step(Gclhigh)
+%step(Gclhigh)
 title("Step response of fail high case")
 %print('report/img/p2-step-fail-high','-dpng');
 %% 8 Step response
 %lsim(Gcl, ones(length(0:0.01:10),1)', 0:0.01:10)
+close all
+eps = 0;
+%fn = @(qin, t, xt) (C_d*A_d*sqrt(2*g))/A*(sqrt(x_r)-sqrt(x_r+xt)) + qin/A;
+Kp = 1;
+Ki = 1;
+fn = @(eps, t, y) [0 y(2) 0;0 0 y(3); -Kp (-1/m*(k+3*eps*y(1)^2+Ki)) -d/m];
+fn_ode = @(t,y) fn(eps,t,y);
+[to, yo] = ode45(fn, [0 1], [0 0 0])
